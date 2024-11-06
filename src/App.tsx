a244faef01f32b26eeab977bf802e3d367cadc31
ITboo <annaacdc@tut.by>
2024-11-06 17:09:11 +0300

    init: setup project, add routing, 404 and header component

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes, BrowserRouter as Router, } from 'react-router-dom';
+import { Suspense } from 'react';
+
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+import './scss/app.scss';
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+    <Route path="/" element={<MainLayout />}>
+      <Route path="" element={<Home />} />
+      <Route
+        path="*"
+        element={
+          <Suspense fallback={<div>Идёт загрузка...</div>}>
+            <NotFound />
+          </Suspense>
+        }
+      />
+    </Route>
+  </Routes>
+  </Router>
+  )
+}
+
+export default App
---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type CategoriesProps = {
+  value: number;
+  onChangeCategory: (idx: number) => void;
+};
+
+const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+export const Categories: React.FC<CategoriesProps> = () => {
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categoryName, i) => (
+          <li key={i}>
+            {categoryName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
